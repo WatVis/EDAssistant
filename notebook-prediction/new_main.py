7b3de64870bca7f471cec197d7696c634964283e
Ecocytus <Ecocytus@github.com>
2021-07-03 08:51:13 +0000

    WIP: adding clf

--- notebook-prediction/new_main.py
+++ notebook-prediction/new_main.py
@@ -8,6 +8,8 @@ import torch
 from torch.utils.data import random_split, Dataset, DataLoader, SequentialSampler, RandomSampler,TensorDataset
 from tqdm import tqdm, trange
 from torch.nn import CrossEntropyLoss, MSELoss
+import torch.nn as nn
+import torch.nn.functional as F
 import multiprocessing
 from transformers import (WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup,
                   RobertaConfig, RobertaModel, RobertaTokenizer)
@@ -16,11 +18,17 @@ import json
 from config import *
 from utils import *
 
+from gensim.models.doc2vec import Doc2Vec
+import tokenize
+from io import BytesIO
+
 ### TODO: move to utils
 
 if __name__ == "__main__":
-    mode = 'embed_doc2vec'
+    mode = 'train_clf'
     data_type = 'train'
+    # model_type = 'doc2vec'
+    model_type = 'codeBERT'
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     class customDataset(Dataset):
         def __init__(self, file_path):
@@ -61,17 +69,37 @@ if __name__ == "__main__":
             return self.__getFromFeatures(self.feautres[idx])
 
     class genDataset(Dataset):
-        def __init__(self):
-            df = pd.read_csv("{}_loc_dataset.csv".format(data_type))
+        def __init__(self, df_file, embed_file, with_libs=False):
+            df = pd.read_csv(df_file)
             # TODO: filter out some competitions to be the training set
-            self.embed_arr = np.load("train_split4/{}_embed_list_total.npy".format(data_type))
+            self.embed_arr = np.load(embed_file)
             split_idx = df.index[df['cell_no'] == 0].tolist()
             self.cell_idx = []
             # filter out those length = 1
             for idx in range(len(split_idx) - 1):
                 start, end = split_idx[idx], split_idx[idx+1]-1
-                if end - start > 0:
+                if end - start > 0 and end - start < 12:
                     self.cell_idx.append((start, end))
+
+            self.with_libs = with_libs
+            if self.with_libs:
+                self.lib_dict = pickle.load(open("lib_dict_new.pkl",'rb'))
+                self.lib_names = df['usages'].to_list()
+                self.lib_count = 19453
+                def get_actual_libs(usage):
+                    actual_usages = usage.split(', ')
+                    actual_libs = []
+                    for idx in range(0, len(actual_usages), 2):
+                        actual_libs.append("{}.{}".format(actual_usages[idx], actual_usages[idx+1]))
+                    return actual_libs
+                self.lib_names = [get_actual_libs(lib) for lib in self.lib_names]
+                
+
+        def getMultiLabel(self, lib_list):
+            label = np.zeros(self.lib_count)
+            for lib in lib_list:
+                label[self.lib_dict[lib]] = 1
+            return label.reshape((1, self.lib_count))
 
         def __len__(self):
             return len(self.cell_idx)
@@ -79,7 +107,11 @@ if __name__ == "__main__":
         def __getitem__(self, idx):
             start, end = self.cell_idx[idx]
             notebook_embeds = self.embed_arr[start:end]
-            return np.vstack([np.zeros((1,768)), notebook_embeds])
+            if self.with_libs:
+                libs = np.concatenate([self.getMultiLabel(lib) for lib in self.lib_names[start:end]])
+                return np.vstack([np.zeros((1,768)), notebook_embeds]), np.vstack([np.zeros((1, self.lib_count)), libs])
+            else:
+                return np.vstack([np.zeros((1,768)), notebook_embeds])
     
     if mode == 'parse':
         print("start parsing...")
@@ -209,7 +241,6 @@ if __name__ == "__main__":
         np.save("train_split4/train_embed_list_total", final)
 
     if mode == 'embed_doc2vec':
-        from gensim.models.doc2vec import Doc2Vec
         def tokenize_code(code, cell_type="code"):
             # if markdown or raw, split with " "
             if cell_type != "code":
@@ -245,7 +276,7 @@ if __name__ == "__main__":
                 return []
         
         df = pd.read_csv("{}_loc_dataset.csv".format(data_type))
-        model = Doc2Vec.load("doc2vec_model.model")
+        model = Doc2Vec.load("doc2vec_model/notebook-doc2vec-model-apr24.model")
         embed_list = []
 
         for index, row in tqdm(df.iterrows(), total=df.shape[0]):
@@ -254,15 +285,12 @@ if __name__ == "__main__":
             usages = row["usages"]
             source = ''.join(extractLoc(readNotebookWithNoMD(competition, kernel_id), row['loc']))
 
-            embed = model.infer_vector(tokenize_code(source))
+            embed = model.infer_vector(tokenize_code(source)).reshape(1, 768)
             embed_list.append(embed)
 
         final_arr = np.concatenate(embed_list, axis=0)
         print(final_arr.shape)
-
-    if mode == "111":
-        df = pd.read_csv("{}_loc_dataset.csv".format(data_type))
-        print(np.unique(df["competition"]))
+        np.save("train_embed_list_doc2vec", final_arr)
 
     if mode == 'train_gen':
         def collate_fn_padd(batch):
@@ -320,7 +348,10 @@ if __name__ == "__main__":
                     total += 1
             return total_loss / total
 
-        dataset = genDataset()
+        df_file = "{}_loc_dataset.csv".format(data_type)
+        # embed_file = "train_split4/{}_embed_list_total.npy".format(data_type)
+        embed_file = "{}_embed_list_{}.npy".format(data_type, model_type)
+        dataset = genDataset(df_file, embed_file)
         train_size = int(len(dataset) * 0.7)
         valid_size = int(len(dataset) * 0.2)
         test_size = len(dataset) - train_size - valid_size
@@ -329,14 +360,15 @@ if __name__ == "__main__":
         train_loader = DataLoader(train_dataset, batch_size=32, collate_fn=collate_fn_padd, shuffle=True)
         valid_loader = DataLoader(valid_dataset, batch_size=32, collate_fn=collate_fn_padd, shuffle=False)
 
-        save_path = "./gen_saved"
+        # save_path = "./gen_saved"
+        # save_path = "./gen_ranked_new"
+        # save_path = "./gen_cosine_new2"
+        save_path = "./gen_cosine_new_doc2vec"
 
         gen = Generator(768, 768).to(device)
         # gen = torch.load(save_path + "/last_gen.pt")
         optimizer_gen = torch.optim.Adam(gen.parameters(), lr=2e-5) 
         eval_loss_list = []
-
-        data, lengths = next(iter(valid_loader))
 
         for epoch_no in range(100):
             print("################TRAIN #{} EPOCH################".format(epoch_no))
@@ -352,14 +384,125 @@ if __name__ == "__main__":
             print("best eval loss is ", min(eval_loss_list))
             # torch.save(gen, save_path + "/last_gen.pt")
             torch.save(gen.state_dict(), save_path + "/last_gen_state_dict.pt")
-        
 
-    if mode == 'valid':
+    if mode == "create_clf_dict":
+        df = pd.read_csv("{}_loc_dataset.csv".format(data_type))
+        lib_names = df['usages'].to_list()
+        
+        def get_actual_libs(usage):
+            actual_usages = usage.split(', ')
+            actual_libs = []
+            for idx in range(0, len(actual_usages), 2):
+                actual_libs.append("{}.{}".format(actual_usages[idx], actual_usages[idx+1]))
+            return actual_libs
+        lib_names = [get_actual_libs(lib) for lib in lib_names]
+
+        lib_dict = {}
+
+        count = 0
+        for libs in lib_names:
+            for lib in libs:
+                if lib not in lib_dict:
+                    lib_dict[lib] = count
+                    count += 1
+
+        print(count)
+
+        pickle.dump(lib_dict,open("lib_dict_new.pkl",'wb'))
+
+    if mode == 'train_clf':
+        def collate_fn_padd(batch):
+            ## padd
+            lengths = torch.IntTensor([ embed.shape[0] for embed, _ in batch ]).to(device)
+            lengths, perm_index = lengths.sort(0, descending=True)
+            embed = torch.nn.utils.rnn.pad_sequence([ torch.Tensor(embed).to(device) for embed, _ in batch ])
+            embed = embed[:, perm_index, :]
+            lib_name = torch.nn.utils.rnn.pad_sequence([ torch.as_tensor(lib_name, dtype=torch.float, device=device) for _, lib_name in batch ])
+            lib_name = lib_name[:, perm_index, :]
+            return embed, lib_name, lengths
+
+        criterion = nn.BCEWithLogitsLoss(reduction="sum")
+
+        def train_clf(embed, lib_name, model, optimizer, lengths):
+            model.zero_grad()
+            model.train()
+            loss = model(embed, lib_name, lengths.cpu(), criterion)
+            loss.backward()
+            optimizer.step()
+            return loss.item()
+
+        def train_iters(loader, model, optimizer, step_print=50):
+            count = 0
+            total = 0
+            total_loss = 0
+            for embed, lib_name, lengths in loader:
+                loss = train_clf(embed, lib_name, model, optimizer, lengths)
+                count += 1
+                total_loss += loss
+                total += 1
+                if count % step_print == 0:
+                    count = 0
+                    # logger.info("cur loss is {}".format(loss))
+            return total_loss / total
+
+        def eval(loader, model):
+            model.eval()
+            total_loss = 0
+            total = 0
+            for embed, lib_name, lengths in loader:
+                with torch.no_grad():
+                    loss = model(embed, lib_name, lengths.cpu(), criterion)
+                    total_loss += loss.item()
+                    total += 1
+            return total_loss / total
+
+        print("creating dataset...")
+        df_file = "{}_loc_dataset.csv".format(data_type)
+        # embed_file = "train_split4/{}_embed_list_total.npy".format(data_type)
+        embed_file = "{}_embed_list_{}.npy".format(data_type, model_type)
+        dataset = genDataset(df_file, embed_file, with_libs=True)
+        train_size = int(len(dataset) * 0.7)
+        valid_size = int(len(dataset) * 0.2)
+        test_size = len(dataset) - train_size - valid_size
+
+        train_dataset, valid_dataset, test_dataset = random_split(dataset, [train_size, valid_size, test_size], generator=torch.Generator().manual_seed(0))
+        train_loader = DataLoader(train_dataset, batch_size=32, collate_fn=collate_fn_padd, shuffle=True)
+        valid_loader = DataLoader(valid_dataset, batch_size=32, collate_fn=collate_fn_padd, shuffle=False)
+
+        save_path = "./clf_saved_codeBERT"
+
+        gen = Generator(768, 768).to(device)
+        # gen = torch.load("./gen_consine/best_gen.pt").to(device)
+        clf = LibClassifier(gen, 768, 19453).to(device)
+        # clf.load_state_dict(torch.load('./clf_saved_new/best_clf_state_dict.pt'))
+        # clf = torch.load("./clf_saved/best_clf.pt").to(device)
+        optimizer = torch.optim.Adam(clf.parameters(), lr=1e-4) 
+
+        eval_loss_list = []
+
+        print("training clf model...")
+
+        for epoch_no in range(200):
+            print("################TRAIN #{} EPOCH################".format(epoch_no))
+            train_loss = train_iters(train_loader, clf, optimizer)
+            print("train loss is: ", train_loss)
+            eval_loss = eval(valid_loader, clf)
+            if len(eval_loss_list) == 0 or eval_loss > max(eval_loss_list):
+                print("Best eval, saved to disc")
+                # torch.save(clf, save_path + "/best_clf.pt")
+                torch.save(clf.state_dict(), save_path + "/best_clf_state_dict.pt")
+            eval_loss_list.append(eval_loss)
+            print("eval accuracy is: ", eval_loss)
+            print("best eval accuracy is ", max(eval_loss_list))
+            # torch.save(clf, save_path + "/last_clf.pt")
+            torch.save(clf.state_dict(), save_path + "/last_clf_state_dict.pt")
+
+    if mode == 'valid_gen':
         class validDataset(Dataset):
-            def __init__(self):
-                df = pd.read_csv("{}_loc_dataset.csv".format(data_type))
+            def __init__(self, df_file, embed_file):
+                df = pd.read_csv(df_file)
                 # TODO: filter out some competitions to be the training set
-                self.embed_arr = np.load("train_split4/{}_embed_list_total.npy".format(data_type))
+                self.embed_arr = np.load(embed_file)
                 split_idx = df.index[df['cell_no'] == 0].tolist()
                 self.cell_idx = []
                 # filter out those length = 1
@@ -376,16 +519,22 @@ if __name__ == "__main__":
                 notebook_embeds = self.embed_arr[start:end]
                 return (np.vstack([np.zeros((1,768)), notebook_embeds]), start, end)
 
+        df_file = "{}_loc_dataset.csv".format(data_type)
+        # embed_file = "train_split4/{}_embed_list_total.npy".format(data_type)
+        embed_file = "{}_embed_list_{}.npy".format(data_type, model_type)
+
         # gen = torch.load("./gen_consine/best_gen.pt").to(device)
         gen = Generator(768, 768).to(device)
-        gen.load_state_dict(torch.load('./gen_saved/best_gen_state_dict.pt'))
+        model_path = "./gen_cosine_new_doc2vec"
+        # model_path = "./gen_cosine_new"
+        gen.load_state_dict(torch.load('{}/best_gen_state_dict.pt'.format(model_path)))
         gen.eval()
         # model.eval()
         print('start validating')
         # df = pd.read_csv("{}_loc_dataset.csv".format(data_type))
         # embed_arr = torch.from_numpy(np.load("train_split4/{}_embed_list_total.npy".format(data_type))).to(device)
         # split_idx = df.index[df['cell_no'] == 0].tolist()
-        dataset = validDataset()
+        dataset = validDataset(df_file, embed_file)
         train_size = int(len(dataset) * 0.7)
         valid_size = int(len(dataset) * 0.2)
         test_size = len(dataset) - train_size - valid_size
@@ -409,18 +558,21 @@ if __name__ == "__main__":
                 for idx in range(2, length):
                     predict_embed = gen.valid_embedding(notebook_embeds[:idx])
                     actual_embed = notebook_embeds[idx]
+                    # TODO: check if notebook_embeds[idx] == embed_arr[start+idx]
+                    # print(notebook_embeds[idx].mean(), embed_arr[start+idx-1].mean())
                     # predict_embed = torch.randn(1, 768).to(device)
                     result = torch.argsort(torch.einsum("ij,ij->i",embed_arr,predict_embed), descending=True).detach().cpu().numpy()
                     # print(predict_embed.std())
-                    rank_list.append(np.where(result == start + idx)[0][0])
+                    rank_list.append(np.where(result == start + idx-1)[0][0])
+                    # print(rank_list[-1])
                     # actual_meta = df.loc[start + idx]
                     # print(actual_meta)
                 # print(rank_list)
         rank_list = np.array(rank_list)
         print(np.mean(rank_list))
-        np.save('./{}_rank_list'.format(data_type), rank_list)
+        np.save('./{}_rank_list_valid_{}'.format(data_type, model_type), rank_list)
 
-    if mode == 'inference':
+    if mode == 'inference_gen':
         print("start inferencing...")
         topn = 5
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -489,9 +641,52 @@ if __name__ == "__main__":
                     print("{}, {}".format(competition, kernel_id))
                     print('\n'.join(extractLoc(raw_source, doc["loc"])))
 
+    if mode == "valid_clf":
+        def collate_fn_padd(batch):
+            ## padd
+            lengths = torch.IntTensor([ embed.shape[0] for embed, _ in batch ]).to(device)
+            lengths, perm_index = lengths.sort(0, descending=True)
+            embed = torch.nn.utils.rnn.pad_sequence([ torch.Tensor(embed).to(device) for embed, _ in batch ])
+            embed = embed[:, perm_index, :]
+            lib_name = torch.nn.utils.rnn.pad_sequence([ torch.as_tensor(lib_name, dtype=torch.float, device=device) for _, lib_name in batch ])
+            lib_name = lib_name[:, perm_index, :]
+            return embed, lib_name, lengths
+
+        criterion = nn.BCEWithLogitsLoss(reduction="sum")
+
+        def eval(loader, model):
+            model.eval()
+            total_loss = 0
+            total = 0
+            for embed, lib_name, lengths in loader:
+                with torch.no_grad():
+                    loss = model(embed, lib_name, lengths.cpu(), criterion)
+                    total_loss += loss.item()
+                    total += 1
+            return total_loss / total
+
+        print("creating dataset...")
+        df_file = "{}_loc_dataset.csv".format(data_type)
+        # embed_file = "train_split4/{}_embed_list_total.npy".format(data_type)
+        embed_file = "{}_embed_list_{}.npy".format(data_type, model_type)
+        dataset = genDataset(df_file, embed_file, with_libs=True)
+        train_size = int(len(dataset) * 0.7)
+        valid_size = int(len(dataset) * 0.2)
+        test_size = len(dataset) - train_size - valid_size
+
+        train_dataset, valid_dataset, test_dataset = random_split(dataset, [train_size, valid_size, test_size], generator=torch.Generator().manual_seed(0))
+        test_loader = DataLoader(valid_dataset, batch_size=32, collate_fn=collate_fn_padd, shuffle=False)
+
+        model_pth = "./clf_saved_new"
+        gen = Generator(768, 768).to(device)
+        clf = LibClassifier(gen, 768, 16855).to(device)
+        clf.load_state_dict(torch.load('./{}/best_clf_state_dict.pt').format(model_pth))
+        clf.eval()
+
+        print(eval(test_loader, clf))
 
 
-    if mode == 'valid_clf':
+    if mode == 'valid_clf_old':
         print("loading model...")
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         config = RobertaConfig.from_pretrained(config_name if config_name else model_name_or_path)
